954eb35cab1ef0fa444a1e859ca67dfaabbf0cac
jackie kircher <kircher.jackie@gmail.com>
2014-04-06 18:47:04 -0400

    create a rails project that will handle data importing
    the application is dependent on Ruby 2.1.1, Rails 4.0.4, and SQLite3.
    Ruby version requirements are designated in .ruby-version and is
    designed to be handled with RVM.

---
+++ importer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Importer::Application.config.secret_key_base = '9c4c7e244077092fbb9472fa2c1937b6282277ffc7440eae7514bf7c44b7eed72327647fb513aafc24d1514f1fef7320b7de2ed125f7edaf78dd05607c62d7f1'
