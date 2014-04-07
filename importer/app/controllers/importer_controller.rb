1cfd249cff24fc10d866016b1fa6443c8b4f4057
jackie kircher <kircher.jackie@gmail.com>
2014-04-06 23:22:32 -0400

    add some rudimentary error checking to the import action
    while it's true that we expect to inputs to be correct, it still feels
    wrong to have IO without any error catching. this adds some minimal
    error checking for the kinds of errors we might expect
    (ActiveRecord::RecordInvalid and IOError) from our form parsing.

--- importer/app/controllers/importer_controller.rb
+++ importer/app/controllers/importer_controller.rb
@@ -1,15 +1,21 @@
 class ImporterController < ApplicationController
 
   def input_data
-
+    @errors = []
   end
 
   def import_data
     if params[:csv_file].blank?
+      @errors = ["You must submit a file to upload"]
       render :input_data and return
     end
 
-    @importer = CSVImporter.new(params[:csv_file])
-    @importer.persist
+    begin
+      @importer = CSVImporter.new(params[:csv_file])
+      @importer.persist
+
+    rescue ActiveRecord::RecordInvalid, IOError => error
+      @errors << error
+    end
   end
 end
