88b8813fb2b194bd9fcf65f0057bb0baa7c27c8b
jackie kircher <kircher.jackie@gmail.com>
2014-04-06 20:33:09 -0400

    configure database_cleaner
    from here on out I'll need to write tests that check persistance, and so
    I'll also want to clean out the database between each test so that
    previous transactions don't interfere with test results. I chose
    database_cleaner because I'm familiar with this library.

--- importer/spec/spec_helper.rb
+++ importer/spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'database_cleaner'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -24,4 +25,16 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
